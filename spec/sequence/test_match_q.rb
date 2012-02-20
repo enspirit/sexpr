33e7a8fdbdfc02ab8baabb4ac2d855f1eaa9eb8f
Bernard Lambeau <blambeau@gmail.com>
2012-02-20 16:54:21 +0100

    Ensure the spec of Sequence#match?

---
+++ spec/sequence/test_match_q.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+module SexpGrammar
+  describe Sequence, 'match?' do
+
+    let(:alt1){ Terminal.new(nil)          }
+    let(:alt2){ Terminal.new(/^[a-z]+$/)   }
+    let(:rule){ Sequence.new [alt1, alt2]  }
+
+    it 'returns true on match' do
+      rule.should be_match([nil, "hello"])
+    end
+
+    it 'returns false on partial match' do
+      rule.should_not be_match([nil, "hello", "world"])
+    end
+
+    it 'returns false on no match' do
+      rule.should_not be_match([:hello, 12])
+      rule.should_not be_match([])
+      rule.should_not be_match(nil)
+      rule.should_not be_match([nil])
+    end
+
+  end
+end
