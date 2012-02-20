7bff37c7b881ed49dc3880528d95af3d6db88902
Bernard Lambeau <blambeau@gmail.com>
2012-02-20 16:43:21 +0100

    Remove pending...

--- spec/test_bool_expr.rb
+++ spec/test_bool_expr.rb
@@ -5,18 +5,14 @@ module SexpGrammar
     let(:g){ SexpGrammar.load(Path.dir/"bool_expr.yml") }
 
     it "allows checking validy of specific nodes" do
-      pending{
-        (g[:bool_lit] === true).should be_true
-      }
+      (g[:bool_lit] === true).should be_true
       (g[:bool_and] === [:bool_and, true, false]).should be_true
       (g[:bool_and] === [:bool_or, true, false]).should be_false
     end
 
     it 'allows checking the validity against the root rule' do
-      pending{
-        (g === true).should be_true
-        (g === false).should be_true
-      }
+      (g === true).should be_true
+      (g === false).should be_true
       (g === [:bool_not, true]).should be_true
       (g === [:var_ref, "x"]).should be_true
       (g === [:bool_not, [:var_ref, "x"]]).should be_true
