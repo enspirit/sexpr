3b384c8aeac8286cabbe460c14e485a225510c4e
Bernard Lambeau <blambeau@gmail.com>
2012-02-20 16:51:38 +0100

    Ensure the spec of Alternative#match?

---
+++ spec/alternative/test_match_q.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+module SexpGrammar
+  describe Alternative, "match?" do
+
+    let(:alt1){ Terminal.new(nil)            }
+    let(:alt2){ Terminal.new(/^[a-z]+$/)     }
+    let(:rule){ Alternative.new [alt1, alt2] }
+
+    it 'returns true if one matches' do
+      rule.should be_match("hello")
+      rule.should be_match(nil)
+    end
+
+    it 'returns false on no match' do
+      rule.should_not be_match("12")
+      rule.should_not be_match([])
+    end
+
+  end
+end
