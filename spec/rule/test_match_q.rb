1315fe81ce0332a95327b15f72c0e3e704e2d913
Bernard Lambeau <blambeau@gmail.com>
2012-02-20 16:53:10 +0100

    Ensure spec of Rule#match?

---
+++ spec/rule/test_match_q.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+module SexpGrammar
+  describe Rule, 'match?' do
+
+    let(:defn){ Terminal.new(/^[a-z]+$/) }
+    let(:rule){ Rule.new :hello, defn }
+
+    it 'returns true on match' do
+      rule.should be_match([:hello, "hello"])
+    end
+
+    it 'returns false on partial match' do
+      rule.should_not be_match([:hello, "hello", "world"])
+    end
+
+    it 'returns false on no match' do
+      rule.should_not be_match(["hello"])
+      rule.should_not be_match([:hello, 12])
+      rule.should_not be_match([])
+      rule.should_not be_match(nil)
+    end
+
+  end
+end
