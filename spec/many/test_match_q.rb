43f1165e80f1b4e80f2d5063460b1f277cfc52c5
Bernard Lambeau <blambeau@gmail.com>
2012-02-20 16:56:14 +0100

    Ensure spec of Many#match?

---
+++ spec/many/test_match_q.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+module SexpGrammar
+  describe Many do
+
+    let(:term){ Terminal.new(/^[a-z]+$/) }
+    let(:rule){ Many.new term, '+'       }
+
+    it 'returns true on match' do
+      rule.should be_match(["hello"])
+      rule.should be_match(["hello", "world"])
+    end
+
+    it 'returns false on partial match' do
+      rule.should_not be_match(["hello", "world", "12"])
+    end
+
+    it 'returns false on no match' do
+      rule.should_not be_match(["12"])
+      rule.should_not be_match([])
+      rule.should_not be_match(nil)
+    end
+
+  end
+end
