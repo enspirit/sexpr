07f7e39758eb54e9d7cd1af8788ab84386e99be4
Bernard Lambeau <blambeau@gmail.com>
2012-02-22 17:51:48 +0100

    Add a helper to set the grammar on a Rewriter subclass.

--- lib/sexpr/rewriter.rb
+++ lib/sexpr/rewriter.rb
@@ -2,6 +2,14 @@ module Sexpr
   class Rewriter < Processor
     helper SexprCoercions
 
+    def self.grammar(sexpr_grammar)
+      @sexpr_grammar = sexpr_grammar
+    end
+
+    def sexpr_grammar
+      self.class.instance_variable_get(:"@sexpr_grammar") || super
+    end
+
     def copy_and_apply(sexpr)
       sexpr.sexpr_copy do |copy, child|
         copy << (Sexpr===child ? call(child) : child)
